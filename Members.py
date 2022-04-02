ae51823d82eecf1ec48c42ed8c46e2f7c873d66c
sandeep-r-c <79988350+sandeep-r-c@users.noreply.github.com>
2022-04-02 13:40:43 +0200

    Code replaced

--- Members.py
+++ Members.py
@@ -1,11 +1,15 @@
-class Books:
-    def __init__(self,BookID,Booktitle,author_name,publication_company, rented_user, rented_date):
-        self.BookID = BookID
-        self.Booktitle = Booktitle
-        self.author_name = author_name
-        self.publication_company = publication_company
-        self.rented_user = rented_user
-        self.rented_date = rented_date
+# members class
+#
+#del Members
+class Members:
+    penalty_fee = 0
+    notifications = "No notifications"
+    def __init__(self,Member_ID, name, dail_number,notifications, penalty_fee = 0):
+        self.Member_ID = Member_ID
+        self.name = name
+        self.notifications = notifications
+        self.dail_number = dail_number
+        self.penalty_fee = penalty_fee
         
         
     def check_and_create(table_name):
@@ -13,7 +17,7 @@ class Books:
         stmt = "SHOW TABLES LIKE %s"%table_name
         cursor_object.execute(stmt)
         result = cursor_object.fetchone()
-        statement = statement_books = "CREATE TABLE books(BookID varchar(255),BookTitle varchar(255),author_name varchar(255),publication_company varchar(255),Rented_date varchar(255), Rented_user varchar(255))"
+        statement = statement_books = "CREATE TABLE Members(MemberID varchar(255),Member_name varchar(255),contact_number varchar(255),penalty_fees varchar(255),Notifications varchar(255))"
         if result:
             x = 1
             print(f"there is already table named {table_name} exists")
@@ -24,101 +28,7 @@ class Books:
             cursor_object.execute(statement)
             print("successfully created the table")
         
-    
-
-
-
-
-class Librarian(Books):
-    def __init__(self, name, ph_number, librarian_ID, book_object):
-        self.name = name 
-        self.ph_number = ph_number
-        self.librarian_ID = librarian_ID
-        self.BookID = book_object.BookID
-        self.author_name = book_object.author_name
-        self.publication_company = book_object.publication_company
-        self.rented_user = book_object.rented_user
-
-    def check_and_create(table_name):
-        global  x
-        stmt = "SHOW TABLES LIKE %s"%table_name
-        cursor_object.execute(stmt)
-        result = cursor_object.fetchone()
-        statement = statement_librarian = "CREATE TABLE Librarians(Librarian_Id int,Librarian_name varchar(255),phone_number varchar(255))"
-        if result:
-            x = 1
-            print(f"there is already table named {table_name} exists")
-        else:
-            x = 0
-            print(f"there is no table named {table_name}")
-            print("Creating the table......")
-            cursor_object.execute(statement)
-            print("successfully created the table")
-            
-            
-    def add_book(self,book_object):
-        #print(book_object.BookID + " "+ book_object.author_name)
-        cursor_object.execute( "SHOW TABLES LIKE 'books'")
-        result = cursor_object.fetchone()
-        if result:
-                x = 1
-                print(f"there is already table named books exists")
-        else:
-            x = 0
-            print(f"there is no table named books")
-            
-        sql_stmt = "INSERT INTO books (BookID,BookTitle,author_name, publication_company, Rented_date, Rented_user) VALUES(%s, %s,%s, %s, %s, %s)"
-        val = ((book_object.BookID,book_object.Booktitle ,book_object.author_name, book_object.publication_company, book_object.rented_date, book_object.rented_user))
-        cursor_object.execute(sql_stmt, val)
-        cnx.commit()
-        print("it is inserted")
         
-    @staticmethod   
-    def add_manybooks(books):
-        sql_stmt = "INSERT INTO books (BookID, BookTitle,author_name, publication_company, Rented_date, Rented_user) VALUES(%s,%s,%s, %s, %s, %s)"
-        cursor_object.executemany(sql_stmt, books)
-        cnx.commit()
-        print ("Suucessfully Inserted many books")
-        
-    def add_Librarian(self):
-        sql_stmt = "INSERT INTO librarians (Librarian_Id, Librarian_name, phone_number) VALUES(%s, %s, %s)"
-        val = ((self.librarian_ID, self.name,self.ph_number))
-        cursor_object.execute(sql_stmt, val)
-        cnx.commit()
-        print("Librarian is suucessfully added")
-        
-    @staticmethod   
-    def update_bookdetails(book_object):
-        sql_stmt = "UPDATE books SET author_name = %s, BookTitle = %s,publication_company =%s, Rented_date = %s, Rented_user=%s WHERE BookID = %s"
-        data = (book_object.author_name,book_object.Booktitle,book_object.publication_company, book_object.rented_date,book_object.rented_user,book_object.BookID )
-        
-        cursor_object.execute(sql_stmt, data)
-        cnx.commit()
-        print(f"Book with BookID {book_object.BookID} is updated")
-        
-        
-    def update_user(mem_obj):
-    
-        sql_stmt = "UPDATE members SET Member_name = %s, contact_number = %s, Notifications = %s,penalty_fees =%s WHERE MemberID = %s"
-        data = (mem_obj.name,mem_obj.dail_number,mem_obj.notifications, mem_obj.penalty_fee,mem_obj.Member_ID )
-
-        cursor_object.execute(sql_stmt, data)
-        cnx.commit()
-        print(f"Book with MemberID {mem_obj.Member_ID} is updated")
-        
-    def delete_a_member(mem_object):
-        sql_stmt = "DELETE FROM members WHERE MemberID = %s"
-        cursor_object.execute(sql_stmt, (mem_object.Member_ID,))
-        cnx.commit()
-        print(f"Member with MemberID {mem_object.Member_ID} is deleted")
-        
-        
-    def delete_a_book(book_object):
-        sql_stmt = "DELETE FROM books WHERE BookID = %s"
-        cursor_object.execute(sql_stmt, (book_object.BookID,))
-        cnx.commit()
-        print(f"Book with BookID {book_object.BookID} is deleted")
-    
     def iter_row(cursor, size=10):
         while True:
             rows = cursor.fetchmany(size)
@@ -128,46 +38,144 @@ class Librarian(Books):
                 yield row    
     
     
-    def query_avail_books():
+    def available_books():
+        global List_avialble_books
         import pandas as pd
-        list_avaialble = []
+        List_avialble_books = []
+        cols = ["BookID", "Titlename", "Title author", "Publisher","Date", "status"]
         Librarian.iter_row(cursor_object)
         sql_stmt = " SELECT * FROM books WHERE Rented_user = 'available'"
         cursor_object.execute(sql_stmt)
         for row in Librarian.iter_row(cursor_object, 10):
             #print(row)
-            list_avaialble.append(row)
+            List_avialble_books.append(row)
             #print()
-        df_avil_books = pd.DataFrame(data=list_avaialble, columns= cols )
-        return df_avil_books
+        df = pd.DataFrame(data = List_avialble_books, columns=cols)
+        df = df.iloc[:,:-2]
+        print(df)
+        
+        
     
-    def see_all_books():
+    def show_me_thebooks():
         import pandas as pd
-        #global cols
-        List_available = []
+        List_all = []
         sql_stmt = "select * from books"
         cursor_object.execute(sql_stmt)
         row = cursor_object.fetchone()
-        cols = ["BookID", "Titlename", "Title author", "Publisher","Rented Date", "status/Rented User"]
+        cols = ["BookID", "Titlename", "Title author", "Publisher","Date", "status"]
         while row is not None:
                  # In the  while loop block, display the contents of the row and move to the next row until all rows are fetched.
                 #print(row)
                 row = cursor_object.fetchone()
-                List_available.append(row)
+                List_all.append(row)
                 #print()
                 
                 
-        df = pd.DataFrame(data = List_available, columns=cols)
-        df = df.iloc[:-1,:]
+        df = pd.DataFrame(data = List_all, columns=cols)
+        df = df.iloc[:-1,:-2]
         return df
-                
+        
+#     def show_me_thebooks():
+#         return Librarian.show_all_books()
+#     def available_books():
+#         avil_books = Librarian.query_avail_books()
+#         return avil_books
+    def BookID_from_sql():
+        global bookId_sql
+        sql_stmt = "SELECT DISTINCT BookID FROM books"
+        cursor_object.execute(sql_stmt)
+        all_sql_books =  cursor_object.fetchall()
+        bookId_sql = list(list(zip(*all_sql_books))[0])
     
+    
+    def borrow_book(member_object):
+        from datetime import datetime, date, timedelta
+        global lis2_bookid
+        Members.available_books()
+        
+        lis2_bookid = []
+        for x in range(0,len(List_avialble_books)):
+            ids = List_avialble_books[x][0]
+            lis2_bookid.append(ids)
+        while True:
+            try:
         
         
-        
-        
+                borrow_id = input("Please enter the BookID: ")
+                if borrow_id in lis2_bookid:
+                    wanted_index = lis2_bookid.index(borrow_id)
+                    BookID = List_avialble_books[wanted_index][0] 
+                    Booktitle = List_avialble_books[wanted_index][1]
+                    author_name = List_avialble_books[wanted_index][2]
+                    publication_company= List_avialble_books[wanted_index][3]
+                    rented_user = member_object.name                            #List_avialble_books[wanted_index][4]
+                    rented_date =  datetime.today().strftime('%d-%m-%Y') #List_avialble_books[wanted_index][5] 
+                    book_borr = Books(BookID,Booktitle,author_name,publication_company, rented_user, rented_date )
+                    Librarian.update_bookdetails(book_borr)
+                    print(f"Book with title {Booktitle} is issued")
+                    last_day = datetime.today() + timedelta(days = 20 )
+                    print(f"return book by {last_day.day}-{last_day.month}-{last_day.year} to avoid Penalty ")
+                    break
                     
+            except ValueError:
+                
+                print("System did not accepted value, check and try again")
+
+            else:
+                print("Please check the BookID and try again")
+                continue
+            
+    from datetime import datetime
+    def penalty_or_not(borrowed_date,returned_date):
+        global calculate_days
+        import datetime
+        from datetime import datetime
+        borrowed_date  # can be fetched from the database
+        returned_date # can be fetched from the database
+        book_out = datetime.strptime(borrowed_date, '%d-%m-%Y')  #parsing a string into datetime object 
+        book_in = datetime.strptime(returned_date, '%d-%m-%Y') 
+        calculate_days = (book_in -book_out)
+        total_days =  calculate_days.days
+        penalty = 0
+        if (total_days) <= 20:
+            print("Thankyou for returning Book, No penalty")
+            return penalty
+        elif (total_days) > 20:
+            for i in range(20,total_days,5):
+                penalty = i+penalty
+
+            print(f"Final penalty for late submission is {penalty}")
+            return penalty
         
         
-        
+    def return_book():
+        global borrowed_date , returned_date
+        while True:
+            try:
+                return_id = input("Please enter the BookID: ")
+                Members.BookID_from_sql()
+
+                if return_id in bookId_sql:
+
+                    sql_stmt = f"SELECT * FROM books WHERE BookID = '{return_id}'"
+                    cursor_object.execute(sql_stmt)
+                    test_row = cursor_object.fetchone()
+                    #print(cursor_object.fetchone())
+                    test_row = list(test_row)
+                    borrowed_date = test_row[4]
+                    returned_date = datetime.today().strftime('%d-%m-%Y')
+                    test_row[-1] = "available"
+                    test_row[-2] = " "
+                    book_update = Books(test_row[0], test_row[1],test_row[2],test_row[3],test_row[5], test_row[4])
+                    data = (test_row[0], test_row[1],test_row[2],test_row[3],test_row[4],test_row[5])
+                    Librarian.update_bookdetails(book_update)
+                    Members.penalty_or_not(borrowed_date,returned_date)
+                    break
+
+            except ValueError:
+                print("Please enter bookID in digits")
+
+            else:
+                print("Please check the BookID and try again")
+                continue
         
